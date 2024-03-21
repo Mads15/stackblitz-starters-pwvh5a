add6f25e86303a520509c8a065d5d36ed647f06f
Mads15 <madskodeloftet@gmail.com>
2024-03-21 11:57:13 +0100

    javascript start

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello!');
+console.log('Console log:');
+
+// Primitive datatyper:
+// sting "Setning"
+// number 100
+// boolean true/false
+
+// variabler
+
+let age = 35;
+console.log (age);
+console.log (typeof age);
+
+const race = "Human";
+console.log (race);
+console.log (typeof race);
+
+const combo = `${age}, ${race}!`; // = age + ' ' + race + !;
+console.log (combo);
+console.log (typeof combo);
